430ee8ecc1ed4daee844b1113c6251321700c0f2
gullinbursti <code@solutionsmatty.com>
2021-05-25 20:44:20 -0400

    Changing tv to living room

--- ir-send-receive.ino
+++ ir-send-receive.ino
@@ -8,19 +8,30 @@
 
 #define BTN_PIN 5
 
-uint16_t sAddress = 0x707;
+//uint16_t sAddress = 0x707; // SAMSUNG
+uint16_t sAddress = 0x0; // LIVING RM
 uint8_t sRepeats = 0;
-
+  
 // pwr btn
-uint8_t sCommand = 0x2;
+//uint8_t sCommand = 0x2; // SAMSUNG
+uint8_t sCommand = 0xC; // LIVING RM
 
 // mute btn 
-//uint8_t sCommand = 0xF;
+//uint8_t sCommand = 0xF; // SAMSUNG
 
+// src btn
+//uint8_t sCommand = 0x38; // LIVING RM
 
+// vol up
+//uint8_t sCommand = 0x10; // LIVING RM
+
+// vol dn
+//uint8_t sCommand = 0x11; // LIVING RM
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(BTN_PIN, INPUT);
+
+  digitalWrite(BTN_PIN, LOW);
 
   Serial.begin(115200);
   delay(4000);
@@ -36,13 +47,17 @@ void setup() {
 
 
 void loop() {
+//  Serial.println(digitalRead(BTN_PIN));
+  
   if (digitalRead(BTN_PIN) == HIGH) {
+    Serial.print(F("Sending... "));  
     IRData IRSendData;
     IRSendData.address = sAddress;
     IRSendData.command = sCommand;
     IRSendData.flags = IRDATA_FLAGS_EMPTY;
   
-    IRSendData.protocol = SAMSUNG;
+//    IRSendData.protocol = SAMSUNG;
+    IRSendData.protocol = RC6;
     Serial.print(F("Sending... "));
     Serial.print(F("ADDR=0x"));
     Serial.print(sAddress, HEX);
@@ -61,16 +76,17 @@ void loop() {
       Serial.println("IR code too long. Edit IRremoteInt.h and increase RAW_BUFFER_LENGTH");
       
     } else {
-      Serial.println(getProtocolString(IrReceiver.decodedIRData.protocol));
-      Serial.println(" =-= ");
-      Serial.println(IrReceiver.decodedIRData.address, HEX);
-      Serial.println(" =-= ");
-      Serial.println(IrReceiver.decodedIRData.command, HEX);
-      Serial.println(" =-= ");
-      Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX);
-      Serial.println(" =-= ");           
-//      Serial.println();
-//      IrReceiver.printIRResultShort(&Serial);
+      Serial.print("=-= PROTOCOL: ");
+      Serial.print(getProtocolString(IrReceiver.decodedIRData.protocol));
+      Serial.print(" =-= ADDR: 0x");
+      Serial.print(IrReceiver.decodedIRData.address, HEX);
+      Serial.print(" =-= CMD: 0x");
+      Serial.print(IrReceiver.decodedIRData.command, HEX);
+      Serial.print(" =-= RAW: 0x");
+      Serial.print(IrReceiver.decodedIRData.decodedRawData, HEX);
+      Serial.print(" =-= ");           
+      Serial.println();
+      IrReceiver.printIRResultShort(&Serial);
       Serial.println();
       Serial.println();
     }
